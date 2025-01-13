24c71bb2a99e9609ce475eba42f733fd250d9cec
ArYaN <113382989+Aaryanb45@users.noreply.github.com>
2025-01-13 17:41:21 +0530

    Create program1.py

---
+++ LAB1/program1.py
@@ -0,0 +1,13 @@
+#perform addition, sub , mul, dividivsion
+num1=float(input("Enter the number 1"))
+num2=float(input("Enter the number 2"))
+
+addition=num1 + num2
+subtraction=num1 - num2
+division= num1 / num2
+modulus = num1 % num2
+
+print(f"Addition:{addition}")
+print(f"Subtraction:{subtraction}")
+print(f"divivsion:{division}")
+print(f"Modulus:{modulus}")
